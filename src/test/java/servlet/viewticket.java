5836057f9fe1990ad064777d29114b01c67038a3
Abdullah <35055376+theslaveofgod@users.noreply.github.com>
2018-01-05 09:42:52 +0530

    Update viewticket.java
    updated ip to 54.169.240.198

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://54.169.24.209:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.169.240.198:5432/postgres";
 			String username="postgres";
 			String password="";
 						
